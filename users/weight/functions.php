a7d58634996a3268037a42b92b7cd6aeaff860ac
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-13 15:52:42 +0200

    Dodano funkcje dodawania wagi przez usera

--- users/weight/functions.php
+++ users/weight/functions.php
@@ -25,12 +25,12 @@ function getWieghtFunc($userID){
 
     $ID = mysqli_real_escape_string($conn, $userID['id']);
 
-    $query = "SELECT weight, measuredate FROM react_php_user_weight WHERE userid='$ID'";
+    $query = "SELECT weight, date FROM react_php_user_weight WHERE userid='$ID' ORDER BY date";
     $result = mysqli_query($conn,$query);
 
     if ($result){
         
-        if (mysqli_num_rows($result) > 1) {
+        if (mysqli_num_rows($result) >= 1) {
 
             $res = mysqli_fetch_all($result,MYSQLI_ASSOC);
 
@@ -58,6 +58,48 @@ function getWieghtFunc($userID){
         ];
         header("HTTP/1.0 500 Internal Server Error");
         return json_encode($data);
+    }
+
+}
+
+function addWeightFunc($addWeight){
+    
+    global $conn;
+
+    $userid = mysqli_real_escape_string($conn, $addWeight['userid']);
+    $date = date('Y.m.d');
+    $weight = mysqli_real_escape_string($conn, $addWeight['weight']);
+
+
+    if (empty(trim($userid))) {
+
+        return error422('Enter user ID');
+
+    } elseif (empty(trim($weight))) {
+
+        return error422('Enter weight');
+
+    } else {
+        $query = "INSERT INTO react_php_user_weight (userid, weight, date) VALUES ('$userid', '$weight', '$date')";
+        $result = mysqli_query($conn, $query);
+
+        if ($result){
+            
+            $data = [
+                'status' => 201,
+                'message' => 'Weight Added Successfully',
+            ];
+            header("HTTP/1.0 201 Created");
+            return json_encode($data);
+
+        } else {
+            $data = [
+                'status' => 500,
+                'messeage' => $method. 'Internal Server Error',
+            ];
+            header("HTTP/1.0 500 Internal Server Error");
+            return json_encode($data);
+        }
     }
 
 }
