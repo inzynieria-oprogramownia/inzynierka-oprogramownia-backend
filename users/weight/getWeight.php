103d321b8149458fcbdb7a5b105a2210ff1805ec
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-17 20:48:09 +0200

    Dodano wszystko

--- users/weight/getWeight.php
+++ users/weight/getWeight.php
@@ -1,7 +1,7 @@
 <?php 
     error_reporting(0);
     header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Method: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization");
 
     include('functions.php');
