2f40dc9e29458eab48ea53ce00c8bb08d3aa88df
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-17 21:18:00 +0200

    commit

--- users/addUser.php
+++ users/addUser.php
@@ -3,6 +3,7 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With, Accept");
+    header("HTTP/1.1 200 OK")
 
     include('functions.php');
 
