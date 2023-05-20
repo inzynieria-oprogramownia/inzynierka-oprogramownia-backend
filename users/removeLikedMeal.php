320c691ac66d157b8844583b039b9257f24a3f98
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-20 13:02:01 +0200

    korsa kontratak

--- users/removeLikedMeal.php
+++ users/removeLikedMeal.php
@@ -3,7 +3,13 @@
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
     header("Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization");
-
+    header("Access-Control-Max-Age: 86400"); // Dopuszczalny czas buforowania nagłówków CORS
+    
+    // Obsługa żądań OPTIONS (preflight)
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
     include('functions.php');
 
     $method = $_SERVER["REQUEST_METHOD"];
