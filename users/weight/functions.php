8f05bcf58e2943ba566fd308cff2ed5743a2bd53
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-21 14:50:31 +0200

    Dodano update wagi

--- users/weight/functions.php
+++ users/weight/functions.php
@@ -104,5 +104,40 @@ function addWeightFunc($addWeight){
 
 }
 
+function updateWeightFunc($input, $params){
+    global $conn;
+
+    if (!isset($params['id'])){
+        return error422('User id not found in a link');
+    } elseif (empty($params['id'])) {
+        return error422('Enter id');
+    }
+
+    $ID = mysqli_real_escape_string($conn, $params['id']);
+
+    $weight = mysqli_real_escape_string($conn, $input['weight']);
+    $date = date("Y-m-d");
+    
+
+    $query = "UPDATE react_php_user_weight SET weight='$weight' WHERE id='$ID' AND date='$date';";
+    $result = mysqli_query($conn, $query);
+
+    if ($result){
+        $data = [
+            'status' => 200,
+            'message' => 'Weight Updated Successfully',
+        ];
+        header("HTTP/1.0 200 Weight Updated Successfully");
+        return json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Internal Server Error',
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        return json_encode($data);
+    }
+}
+
 
 ?>
---
+++ users/weight/updateWeight.php
@@ -0,0 +1,36 @@
+<?php
+    error_reporting(0);
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Max-Age: 86400"); // Dopuszczalny czas buforowania nagłówków CORS
+
+    // Obsługa żądań OPTIONS (preflight)
+    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+
+    include('functions.php');
+
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    if ($method == 'PUT') {
+        $inputData = json_decode(file_get_contents("php://input"), true);
+
+        if (empty($inputData)) {
+            $updateWeight = updateWeightFunc($_POST, $_GET);
+        } else {
+            $updateWeight = updateWeightFunc($inputData, $_GET);
+        }
+        echo $updateWeight;
+    } else {
+        $data = [
+            'status' => 405,
+            'message' => $method. ' Method Not Allowed',
+        ];
+        header("HTTP/1.0 405 Method Not Allowed");
+        echo json_encode($data);
+    }
+
+?>
