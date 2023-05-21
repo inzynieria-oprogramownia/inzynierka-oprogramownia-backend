889f3c159ff0ea2709e95825eaa4ec60539d9627
Jigokuzuki <maciek.gorski888@wp.pl>
2023-05-21 10:57:06 +0200

    dodano rezygnacje z premium

--- users/premium/functions.php
+++ users/premium/functions.php
@@ -44,4 +44,37 @@ function updatePremiumFunc($input, $params){
 }
 
 
+function removePremiumFunc($input, $params){
+    global $conn;
+
+    if (!isset($params['id'])){
+        return error422('User id not found in a link');
+    } elseif (empty($params['id'])) {
+        return error422('Enter id');
+    }
+
+    $ID = mysqli_real_escape_string($conn, $params['id']);
+
+    $query = "UPDATE react_php_users SET premium = 0 WHERE id='$ID';";
+    $result = mysqli_query($conn, $query);
+
+    if ($result){
+        $data = [
+            'status' => 200,
+            'message' => 'Premium Removed Successfully',
+        ];
+        header("HTTP/1.0 200 Premium Removed Successfully");
+        return json_encode($data);
+    } else {
+        $data = [
+            'status' => 500,
+            'message' => 'Internal Server Error',
+        ];
+        header("HTTP/1.0 500 Internal Server Error");
+        return json_encode($data);
+    }
+}
+
+
+
 ?>
